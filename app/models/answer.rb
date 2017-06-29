4ce8ffe10ddab727196126bf1dce821135c9e333
satong <stephanietong@gmail.com>
2017-06-29 12:18:09 -0700

    Add placeholder files for views and controllers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,9 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+
+  def answer_preview
+    self.body[0..23]
+  end
+
 end
